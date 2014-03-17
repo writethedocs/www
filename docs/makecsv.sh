836a64dcc9b47d25b82f22d060e634cdee7d2326
Eric Holscher <eric@ericholscher.com>
2014-03-17 15:58:07 +0800

    Remove blanks

--- docs/makecsv.sh
+++ docs/makecsv.sh
@@ -1,2 +1,2 @@
-tail -n +2 ~/Downloads/EU\ Schedule\ -\ Schedule.csv | csvcut -c 2,1 |head -n 18 | cut -c1-4,8- > _data/eu2014-day1.csv
-tail -n +2 ~/Downloads/EU\ Schedule\ -\ Schedule.csv | csvcut -c 2,1 |tail -n 18 | cut -c1-4,8- > _data/eu2014-day2.csv
+tail -n +2 ~/Downloads/EU\ Schedule\ -\ Schedule.csv | csvcut -x -c 2,1 |head -n 18 | cut -c1-4,8- > _data/eu2014-day1.csv
+tail -n +2 ~/Downloads/EU\ Schedule\ -\ Schedule.csv | csvcut -x -c 2,1 |tail -n 17 | cut -c1-4,8- > _data/eu2014-day2.csv
