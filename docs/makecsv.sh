198cbf59820daaf37a9983d8cf2c3633b682b95c
Eric Holscher <eric@ericholscher.com>
2014-04-17 14:28:14 -0700

    Rejigger schedule

--- docs/makecsv.sh
+++ docs/makecsv.sh
@@ -2,4 +2,4 @@ tail -n +2 ~/Downloads/EU\ Schedule\ -\ Schedule.csv | csvcut -x -c 2,1 |head -n
 tail -n +2 ~/Downloads/EU\ Schedule\ -\ Schedule.csv | csvcut -x -c 2,1 |tail -n 19 | cut -c1-4,8- > _data/eu2014-day2.csv
 
 tail -n +2 ~/Downloads/NA\ Schedule\ -\ Schedule.csv | csvcut -x -c 2,1 |head -n 25 | cut -c1-4,8- > _data/na2014-day1.csv
-tail -n +2 ~/Downloads/NA\ Schedule\ -\ Schedule.csv | csvcut -x -c 2,1 |tail -n 20 | cut -c1-4,8- > _data/na2014-day2.csv
+tail -n +2 ~/Downloads/NA\ Schedule\ -\ Schedule.csv | csvcut -x -c 2,1 |tail -n 21 | cut -c1-4,8- > _data/na2014-day2.csv
