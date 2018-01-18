782a70b3eb21838439410dd19ffa0cdc08e8d6e8
Samuel Wright <lykoszine@gmail.com>
2018-01-18 19:23:15 -0100

    This didnt do anything

--- docs/conf.py
+++ docs/conf.py
@@ -103,7 +103,7 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
-suppress_warnings = ['image.nonlocal_uri', 'ref.ref', 'ref.doc']
+suppress_warnings = ['image.nonlocal_uri']
 
 # Our additions
 
