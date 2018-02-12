0b6e1f75ce5cf3118bc77fdb826c8e663b0a6681
Eric Holscher <eric@ericholscher.com>
2018-02-12 21:20:24 +0700

    Add speaker yaml data

--- docs/conf.py
+++ docs/conf.py
@@ -113,6 +113,18 @@ html_context = {
 }
 
 
+def trans():
+    from .ext.speakers import *
+    from .ext.core import *
+    import yaml
+
+    conference_data = load_conference_data()
+    dat = conference_data[2018]['portland']['speakers']
+    transform_speakers(dat)
+    yaml.safe_dump(dat, open('_data/2018.portland.speakers.yaml'), default_flow_style=False)
+
+trans()
+
 def setup(app):
     app.connect('html-page-context', override_page_template)
     app.connect("source-read", rstjinja)
