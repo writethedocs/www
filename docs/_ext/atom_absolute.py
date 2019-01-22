6a8623a2d36838286b5c4664f415b3787fb7051b
Eric Holscher <eric@ericholscher.com>
2019-01-22 09:25:12 -0500

    Write more absolute feeds

--- docs/_ext/atom_absolute.py
+++ docs/_ext/atom_absolute.py
@@ -9,6 +9,13 @@ from sphinx.util import logging
 
 logger = logging.getLogger(__name__)
 
+TAGS = [
+    'newsletter',
+    'portland',
+    'prague',
+    'australia',
+]
+
 
 def rewrite_atom_feed(app, exception):
     """
@@ -20,30 +27,35 @@ def rewrite_atom_feed(app, exception):
     else:
         logger.info('Rewriting atom feed...')
 
-        feed_path = os.path.join(app.outdir, 'blog/archive/tag/newsletter/atom.xml')
-        feed_url = urljoin(app.config.blog_baseurl, 'archive/atom.xml')
-        rewritten_feed_path = os.path.join(
-            app.outdir, 'blog/archive/tag/newsletter/atom_absolute.xml'
-        )
+    for tag in TAGS:
+        _rewrite_feed(app, tag)
 
-        if not os.path.isfile(feed_path):
-            logger.error('Atom feed does not exist at: {}'.format(feed_path))
-            return
 
-        doc = etree.parse(feed_path)
-        root = doc.getroot()
+def _rewrite_feed(app, tag):
+    feed_url = urljoin(app.config.blog_baseurl, 'archive/atom.xml')
 
-        # Rewrite the namespace map to not be Null
-        namespace = list(root.nsmap.values())[0]
-        nsmap = {'atom': namespace}
+    feed_path = os.path.join(app.outdir, f'blog/archive/tag/{tag}/atom.xml')
+    rewritten_feed_path = os.path.join(
+        app.outdir, f'blog/archive/tag/{tag}/atom_absolute.xml'
+    )
 
-        # Convert the content nodes to use absolute links
-        for content in root.xpath('//atom:entry/atom:content', namespaces=nsmap):
-            html_content = html.fromstring(content.text)
-            html_content.make_links_absolute(feed_url)
-            content.text = html.tostring(html_content)
+    if not os.path.isfile(feed_path):
+        logger.error('Atom feed does not exist at: {}'.format(feed_path))
+        return
 
-        with open(rewritten_feed_path, 'wb') as fd:
-            doc.write(fd)
-            logger.info('Wrote absolute atom feed to {}'.format(rewritten_feed_path))
+    doc = etree.parse(feed_path)
+    root = doc.getroot()
 
+    # Rewrite the namespace map to not be Null
+    namespace = list(root.nsmap.values())[0]
+    nsmap = {'atom': namespace}
+
+    # Convert the content nodes to use absolute links
+    for content in root.xpath('//atom:entry/atom:content', namespaces=nsmap):
+        html_content = html.fromstring(content.text)
+        html_content.make_links_absolute(feed_url)
+        content.text = html.tostring(html_content)
+
+    with open(rewritten_feed_path, 'wb') as fd:
+        doc.write(fd)
+        logger.info('Wrote absolute atom feed to {}'.format(rewritten_feed_path))
