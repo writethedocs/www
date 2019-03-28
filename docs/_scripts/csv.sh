864c37b1026ff62d3a1bd6ce432592b28c08715e
Eric Holscher <eric@ericholscher.com>
2019-03-28 14:01:35 -0300

    Update schedule with actual talks

--- docs/_scripts/csv.sh
+++ docs/_scripts/csv.sh
@@ -43,5 +43,5 @@
 # Portland 2019
 
 cat cat ~/Downloads/Portland\ 2019\ Schedules\ -\ Writing\ Day.csv | csvjson -I -i 4 | python -c 'import sys, yaml, json; yaml.safe_dump(json.load(sys.stdin), sys.stdout, default_flow_style=False)' > ../_data/portland-2019-writing-day.yaml
-cat ~/Downloads/Portland\ 2019\ Schedules\ -\ Main\ Conference.csv | head -n 25 |csvjson -I -i 4 | python -c 'import sys, yaml, json; yaml.safe_dump(json.load(sys.stdin), sys.stdout, default_flow_style=False)' > ../_data/portland-2019-day-1.yaml
-cat ~/Downloads/Portland\ 2019\ Schedules\ -\ Main\ Conference.csv | tail -n 22 |csvjson -I -i 4 | python -c 'import sys, yaml, json; yaml.safe_dump(json.load(sys.stdin), sys.stdout, default_flow_style=False)' > ../_data/portland-2019-day-2.yaml
+cat ~/Downloads/Portland\ 2019\ Schedules\ -\ Main\ Filled.csv | head -n 25 |csvjson -I -i 4 | python -c 'import sys, yaml, json; yaml.safe_dump(json.load(sys.stdin), sys.stdout, default_flow_style=False)' > ../_data/portland-2019-day-1.yaml
+cat ~/Downloads/Portland\ 2019\ Schedules\ -\ Main\ Filled.csv | tail -n 20 |csvjson -I -i 4 | python -c 'import sys, yaml, json; yaml.safe_dump(json.load(sys.stdin), sys.stdout, default_flow_style=False)' > ../_data/portland-2019-day-2.yaml
