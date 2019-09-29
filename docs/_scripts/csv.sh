e07d817d735a0f27f4fad207e7456d2b065646a8
Samuel Wright <lykoszine@gmail.com>
2019-09-29 16:56:59 +0200

    Schedule

--- docs/_scripts/csv.sh
+++ docs/_scripts/csv.sh
@@ -48,5 +48,10 @@
 
 # Prague 2019
 
-cat ~/Downloads/Prague\ speaker\ confirmations\ 2019.csv | head -n 25 |csvjson -I -i 4 | python -c 'import sys, yaml, json; yaml.safe_dump(json.load(sys.stdin), sys.stdout, default_flow_style=False)' > ../_data/prague-2019-day-1.yaml
-cat ~/Downloads/Prague\ speaker\ confirmations\ 2019.csv | tail -n 18 |csvjson -I -i 4 | python -c 'import sys, yaml, json; yaml.safe_dump(json.load(sys.stdin), sys.stdout, default_flow_style=False)' > ../_data/prague-2019-day-2.yaml
+#cat ~/Downloads/Prague\ speaker\ confirmations\ 2019.csv | head -n 25 |csvjson -I -i 4 | python -c 'import sys, yaml, json; yaml.safe_dump(json.load(sys.stdin), sys.stdout, default_flow_style=False)' > ../_data/prague-2019-day-1.yaml
+#cat ~/Downloads/Prague\ speaker\ confirmations\ 2019.csv | tail -n 18 |csvjson -I -i 4 | python -c 'import sys, yaml, json; yaml.safe_dump(json.load(sys.stdin), sys.stdout, default_flow_style=False)' > ../_data/prague-2019-day-2.yaml
+
+# Australia 2019
+
+cat ~/Downloads/Australia\ 2019\ Schedules\ -\ Main\ Filled.csv | head -n 20 |csvjson -I -i 4 | python -c 'import sys, yaml, json; yaml.safe_dump(json.load(sys.stdin), sys.stdout, default_flow_style=False)' > ../_data/australia-2019-day-1.yaml
+cat ~/Downloads/Australia\ 2019\ Schedules\ -\ Main\ Filled.csv | tail -n 20 |csvjson -I -i 4 | python -c 'import sys, yaml, json; yaml.safe_dump(json.load(sys.stdin), sys.stdout, default_flow_style=False)' > ../_data/australia-2019-day-2.yaml
