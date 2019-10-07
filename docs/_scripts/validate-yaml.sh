649b24cb2087c0bc46d929a54101e417017aeb48
Sasha Romijn <github@mxsasha.eu>
2019-10-07 18:11:43 +0200

    Add validation for >=2020 sessions and schedule

--- docs/_scripts/validate-yaml.sh
+++ docs/_scripts/validate-yaml.sh
@@ -9,4 +9,17 @@ do
 	yamale --strict -s docs/_data/schema-config.yaml $y -p ruamel
 done
 
+# Note that this and the next check only covers 2020 and newer schedules
+for y in ../_data/*-schedule.yaml
+do
+    [[ $y == *"schema"* ]] && continue
+	yamale --strict -s docs/_data/schema-schedule.yaml $y -p ruamel
+done
+
+for y in ../_data/*-sessions.yaml
+do
+    [[ $y == *"schema"* ]] && continue
+	yamale --strict -s docs/_data/schema-sessions.yaml $y -p ruamel
+done
+
 cd -
