8bb494869c534e9f7db7cfcabe560730a7970297
Sasha Romijn <github@mxsasha.eu>
2019-10-07 17:56:38 +0200

    Update config YAML validation and enable strict mode, fix #827
    YAML validation will now fail when encountering unknown keys.
    The `about.sat` setting was removed from Australia 2018/2019 (where it
    was empty) and Portland 2018 (where it was `5`). It seems unused.
    The `about.videos` setting was removed from Prague 2018, because it
    is never actually used.

--- docs/_scripts/validate-yaml.sh
+++ docs/_scripts/validate-yaml.sh
@@ -3,10 +3,10 @@ set -e
 
 cd "$(dirname "$0")"
 
-for y in ../_data/config*
+for y in ../_data/config-* ../_data/*-config.yaml
 do
     [[ $y == *"schema"* ]] && continue
-	yamale -s docs/_data/config-schema.yaml $y -p ruamel
+	yamale --strict -s docs/_data/schema-config.yaml $y -p ruamel
 done
 
 cd -
