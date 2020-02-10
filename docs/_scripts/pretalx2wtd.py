91a336c9b2d25c2db0d92d6a5ebad2bab14a7413
Sasha Romijn <github@mxsasha.eu>
2020-02-10 12:08:24 +0100

    Improve ordering in sessions YAML generated from pretalix

--- docs/_scripts/pretalx2wtd.py
+++ docs/_scripts/pretalx2wtd.py
@@ -1,37 +1,42 @@
 import sys
 sys.path.insert(1, '../_ext')
 
+from collections import OrderedDict
 from utils import slugify
 import json
-from ruamel.yaml import YAML
+from ruamel import yaml
+from ruamel.yaml.representer import RoundTripRepresenter
 import markdown
 
 yamldoc = []
+# Prevent OrderedDict from being presented as YAML OMAP - we just want a regular YAML dict.
+yaml.add_representer(OrderedDict, RoundTripRepresenter.represent_dict, representer=RoundTripRepresenter)
 
 def convert_to_yaml(year, series, slug, pretalx_results, yaml_file):
     with open(pretalx_results) as json_file:
         talks = json.load(json_file)
         for index, talk in enumerate(talks['results']):
-            yamldoc.append({
-                'title': talk['title'],
-                'slug':slugify(talk['title'] + '-' + talk['speakers'][0]['name']),
-                'abstract': markdown.markdown(talk['abstract']),
-                'speakers': [],
-                'series': series,
-                'series_slug': slug,
-                'year': int(year)
+            yamldoc.append(OrderedDict([
+                ('title', talk['title']),
+                ('slug',slugify(talk['title'] + '-' + talk['speakers'][0]['name'])),
+                ('series', series),
+                ('series_slug', slug),
+                ('year', int(year)),
+                ('speakers', []),
+                ('abstract', markdown.markdown(talk['abstract'])),
 
-                })
+            ]))
             for s in talk['speakers']:
-                yamldoc[index]['speakers'].append({
-                    'name': s['name'],
-                    'slug': slugify(s['name']),
-                    'avatar': s['avatar']
-                     })
+                yamldoc[index]['speakers'].append(OrderedDict([
+                    ('name', s['name']),
+                    ('slug', slugify(s['name'])),
+                    ('avatar', s['avatar']),
+                ]))
 
-        yaml=YAML(typ='safe')
-        yaml.default_flow_style = False
-        yaml.dump(yamldoc, open(yaml_file, 'w+'))
+        yaml_obj=yaml.YAML(typ='safe')
+        yaml_obj.Representer = RoundTripRepresenter
+        yaml_obj.default_flow_style = False
+        yaml_obj.dump(yamldoc, open(yaml_file, 'w+'))
 
 
 
