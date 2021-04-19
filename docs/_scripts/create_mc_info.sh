fd50a5eacf7103fffe15f21b255f0ce3136b41ea
Samuel Wright <lykoszine@gmail.com>
2021-04-19 16:23:37 +0200

    Tweak tools

--- docs/_scripts/create_mc_info.sh
+++ docs/_scripts/create_mc_info.sh
@@ -3,14 +3,7 @@
 # List speakers who have not completed all questions in Pretalx
 # See pretalx2mc-info.py for details about the API authentication
 
+# Either uncomment the following line or run python pretalx2mc-info.py separately
 #python pretalx2mc-info.py
-
-# {
-#  "title": "Beyond metrics: Using maturity models to develop a docs strategy",
-#  "speaker": "Sarah R. Rodlund",
-#  "pronouns": "She, Her, Hers / They, Their, Theirs",
-#  "facts": "At the moment, in no specific order, my favorite things are bicycles, the Pacific Ocean, hanging out with my elderly cat, Otis, learning Spanish, and of course, writing!.",
-#  "pronounce": "Exactly like the English version here: https://translate.google.com/?hl=en&sl=en&tl=sv&text=Rod%20Lund&op=translate"
-#}
 
 yq -r '.[] | {title: .title, speaker: .speakers[].name, pronouns: .speakers[].pronouns, facts: .speakers[].facts, pronounce: .speakers[].pronounce} | "## "+.speaker + " ("+ .pronouns+")\n### "+ .title +"\n\n"+ .facts +"\n\nName pronunciation guide: "+ .pronounce +"\n"' ../_data/mc-info.yaml > mc-info.md
--- docs/_scripts/show_speakers_without_info.sh
+++ docs/_scripts/show_speakers_without_info.sh
@@ -3,5 +3,6 @@
 # List speakers who have not completed all questions in Pretalx
 # See pretalx2mc-info.py for details about the API authentication
 
-python pretalx2mc-info.py
+# Either uncomment the following line or run python pretalx2mc-info.py separately
+#python pretalx2mc-info.py
 yq '.[].speakers[] | select(.pronouns == null) | .name' ../_data/mc-info.yaml
