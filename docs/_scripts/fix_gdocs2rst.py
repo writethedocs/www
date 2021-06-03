a3793d703cad25160fda2f741fce35d58083ab40
Samuel Wright <lykoszine@gmail.com>
2021-06-03 14:39:51 +0200

    Survey results take 1

---
+++ docs/_scripts/fix_gdocs2rst.py
@@ -0,0 +1,24 @@
+in_rubric = False
+
+
+with open('/home/samuel/Downloads/WIP.rst') as fp:
+    for cnt, line in enumerate(fp):
+       #print("Line {}: {}".format(cnt, line))
+
+       if line == "+-----------------------------------------------------------------------+\n" and not in_rubric:
+         in_rubric = True
+       elif in_rubric and line.startswith('| .. rubric:: '):
+         #line = ".. container:: "+line[14:-2]
+         print(".. container:: question")
+       elif in_rubric and line.startswith('|    :'):
+         line = line[2:-2]
+         #print(line)
+       elif line == "+-----------------------------------------------------------------------+\n" and in_rubric:
+         in_rubric = False
+       # Default last   
+       elif in_rubric:
+         line = "   "+line[2:-2]
+         print(line)
+       elif not in_rubric:
+         print(line, end="")
+         
