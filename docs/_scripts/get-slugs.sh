aadc7423ff8143128a86c41cdd8b15767d356a25
Plaindocs <sam@writethedocs.org>
2021-06-15 20:30:12 +0200

    Survey results take 1 (#1545)
    Salary Survey 2020 Results 🎉 
    
    Co-authored-by: Kay Smoljak <kay.smoljak@gmail.com>
    Co-authored-by: Mikey Ariel <thatdocslady@users.noreply.github.com>
    Co-authored-by: Eric Holscher <25510+ericholscher@users.noreply.github.com>

--- docs/_scripts/get-slugs.sh
+++ docs/_scripts/get-slugs.sh
@@ -2,6 +2,6 @@
 
 # Print the slugs from the sessions so you can paste them into the schedule
 
-yq '.[].slug' _data/portland-2021-sessions.yaml | tr -d '"'
+yq '.[].slug' ../_data/portland-2021-sessions.yaml | tr -d '"'
 
 
