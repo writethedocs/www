03a3d21ddfdca95454246aaf4d2d371a320f2d58
Sasha Romijn <github@mxsasha.eu>
2022-06-27 17:04:30 +0200

    Produce complete CSV with mean,median,pvar

--- docs/_scripts/pretalx2cfpsheet.py
+++ docs/_scripts/pretalx2cfpsheet.py
@@ -1,60 +1,77 @@
-# Generate a sessions YAML from Pretalx talk selections.
+# Generate a CFP review sheet from Pretalx
 # To use:
 # - Get an API token from https://pretalx.com/orga/me
 # - Update the parameters at the end of this file, if needed (e.g. conference name)
 # - Do: export PRETALX_TOKEN=<your token>
 # - Go to directory docs/_scripts
 # - Run this script
-import shutil
-import sys
-sys.path.insert(1, '../_ext')
-
 import os
-from collections import OrderedDict
+import csv
 import requests
+import statistics
 
-def get_review_scores(pretalx_slug):
+
+def get_review_scores(pretalx_slug, output_file):
     if not os.environ.get('PRETALX_TOKEN'):
         print('Error: PRETALX_TOKEN not found in environment variables.')
         return
     http_headers = {'Authorization': 'Token ' + os.environ['PRETALX_TOKEN']}
 
-    submissions_url = f'https://pretalx.com/api/events/{pretalx_slug}/submissions/?state=submitted'
+    submissions_url = f'https://pretalx.com/api/events/{pretalx_slug}/submissions/'
     print(f'Loading submissions from {submissions_url}...')
-
-    while(submissions_url):
-        submissions = requests.get(submissions_url, headers=http_headers)
-        if submissions.status_code != 200:
-            print(f'Error: submissions request failed: {submissions.status_code}: {submissions.text}')
-            return
-
-        for index, talk in enumerate(submissions.json()['results']):
-            print(talk['code']+'    '+talk['title'])
-
-        if submissions.json()['next']:
-            submissions_url = submissions.json()['next']
-        else:
-            submissions_url=False
+    submissions_list = load_pretalx_resource(submissions_url, http_headers)
+    submissions = {submission['code']: submission for submission in submissions_list}
 
     reviews_url = f'https://pretalx.com/api/events/{pretalx_slug}/reviews'
-    print(f'Loading reviews from {reviews_url}...')
 
-    while(reviews_url):
-        reviews = requests.get(reviews_url, headers=http_headers)
-        if reviews.status_code != 200:
-            print(f'Error: reviews request failed: {reviews.status_code}: {reviews.text}')
+    reviews = load_pretalx_resource(reviews_url, http_headers)
+    for review in reviews:
+        try:
+            submissions[review['submission']].setdefault("scores", []).append(float(review['score']))
+        except KeyError:
+            print(f"WARNING: Ignoring review {review['id']} for unknown submission {review['submission']}")
+        except TypeError:
+            print(f"WARNING: Ignoring review {review['id']} for missing/invalid score {review['score']}")
+
+    with open(output_file, 'w') as f:
+        csvwriter = csv.writer(f)
+        csvwriter.writerow(["Mean", "Median", "Pvar", "Title", "Speaker", "Tags", "URL"])
+        for code, submission in submissions.items():
+            speakers = ', '.join([speaker['name'] for speaker in submission['speakers']])
+            url = f"https://pretalx.com/orga/event/{pretalx_slug}/submissions/{code}/reviews/"
+            scores = submission.get('scores', [0])
+            csvwriter.writerow([
+                "%.1f" % statistics.mean(scores),
+                "%.1f" % statistics.median(scores),
+                "%.1f" % statistics.pvariance(scores),
+                submission['title'],
+                speakers,
+                ', '.join(submission['tags']),
+                url,
+            ])
+    print(f'Wrote {len(submissions)} submissions to {output_file}')
+
+
+# Might be reusable in other pretalx interfaces
+def load_pretalx_resource(url, http_headers):
+    results = []
+    while url:
+        response = requests.get(url, headers=http_headers)
+        if response.status_code != 200:
+            print(f'Error: request failed: {response.status_code}: {response.text}')
             return
 
-        for index, review in enumerate(reviews.json()['results']):
-            if isinstance(review['score'], str):
-                print(review['submission']+'    '+review['score'])
+        results += response.json()['results']
 
-        if reviews.json()['next']:
-            reviews_url = reviews.json()['next']
+        if response.json()['next']:
+            url = response.json()['next']
         else:
-            reviews_url=False
+            break
+    return results
+
 
 if __name__ == '__main__':
     get_review_scores(
-        pretalx_slug='wtd-portland-2022'
+        pretalx_slug='wtd-portland-2022',
+        output_file='wtd-portland-2022-reviews.csv',
     )
