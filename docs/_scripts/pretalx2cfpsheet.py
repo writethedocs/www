201c0cf2d6094c4b3f8b1435b8fa347aa1a716ed
Sasha Romijn <github@mxsasha.eu>
2022-06-28 11:44:58 +0200

    Add pretalx query for all submissions

--- docs/_scripts/pretalx2cfpsheet.py
+++ docs/_scripts/pretalx2cfpsheet.py
@@ -6,9 +6,53 @@
 # - Go to directory docs/_scripts
 # - Run this script
 import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), "_ext"))
+
 import csv
 import requests
 import statistics
+
+
+def get_all_speakers(pretalx_slugs, output_file):
+    if not os.environ.get('PRETALX_TOKEN'):
+        print('Error: PRETALX_TOKEN not found in environment variables.')
+        return
+    http_headers = {'Authorization': 'Token ' + os.environ['PRETALX_TOKEN']}
+
+    events_list = []
+    for slug in pretalx_slugs:
+        url = f'https://pretalx.com/api/events/{slug}'
+        print(f'Loading event from {url}...')
+        response = requests.get(url, headers=http_headers)
+        if response.status_code != 200:
+            print(f'Error: request failed: {response.status_code}: {response.text}')
+            return
+        events_list.append(response.json())
+
+    submissions_list = []
+    for event in events_list:
+        submissions_url = f'https://pretalx.com/api/events/{event["slug"]}/submissions/'
+        print(f'Loading submissions from {submissions_url}...')
+        event_submissions = load_pretalx_resource(submissions_url, http_headers)
+        for submission in event_submissions:
+            submission['event'] = event['name']['en']
+            submission['year'] = event['date_from'].split('-')[0]
+        submissions_list += event_submissions
+
+    with open(output_file, 'w') as f:
+        csvwriter = csv.writer(f)
+        csvwriter.writerow(["Speaker", "Talk", "Conference", "Year", "Status"])
+        for submission in submissions_list:
+            speakers = ', '.join([speaker['name'] for speaker in submission['speakers']])
+            csvwriter.writerow([
+                speakers,
+                submission['title'],
+                submission['event'].replace(submission['year'], '').strip(),
+                submission['year'],
+                submission['state'],
+            ])
+    print(f'Wrote {len(submissions_list)} submissions to {output_file}')
 
 
 def get_review_scores(pretalx_slug, output_file):
@@ -71,7 +115,20 @@ def load_pretalx_resource(url, http_headers):
 
 
 if __name__ == '__main__':
-    get_review_scores(
-        pretalx_slug='wtd-portland-2022',
-        output_file='wtd-portland-2022-reviews.csv',
+    # get_review_scores(
+    #     pretalx_slug='wtd-portland-2022',
+    #     output_file='wtd-portland-2022-reviews.csv',
+    # )
+    get_all_speakers(
+        pretalx_slugs=[
+            'wtd-portland-2020',
+            'write-the-docs-portland-2021',
+            'wtd-portland-2022',
+            'wtd-prague-2020',
+            'write-the-docs-prague-2021',
+            'wtd-prague-2022',
+            'write-the-docs-australia-2020',
+            'wtd-australia-and-india-2021',
+        ],
+        output_file='wtd-speakers.csv',
     )
