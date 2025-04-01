834bb4a8a5f44aa2fb757913a5e93620df8b91e6
Kay Smoljak <kay.smoljak@gmail.com>
2025-04-01 10:10:21 +0200

    table sorting accessibility improvements

--- docs/_static/survey/js/survey.js
+++ docs/_static/survey/js/survey.js
@@ -1,21 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // Select all submenu links, whether inside <p> or directly inside <li>
+  
+  /* internal navigation */
   var submenuLinks = document.querySelectorAll(".simple > li > ul > li a");
-
   submenuLinks.forEach(function(link) {
     link.addEventListener("click", function(event) {
-      var parentLi = this.closest("li"); // Find the closest <li> instead of assuming direct parent
+      var parentLi = this.closest("li"); 
       var siblings = parentLi.parentElement.children;
-
       // Hide other submenus
       for (var i = 0; i < siblings.length; i++) {
         if (siblings[i] !== parentLi) {
           siblings[i].classList.remove("active");
         }
       }
-
       // Toggle current submenu
       parentLi.classList.toggle("active");
     });
   });
+
 });
