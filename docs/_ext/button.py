a74cb89ea1c15a100f7da2ca27f7cb76acf40f09
Eric Holscher <eric@ericholscher.com>
2026-04-07 17:25:39 +0800

    Replace inline HTML buttons in one-month-out post, clean up f-string

--- docs/_ext/button.py
+++ docs/_ext/button.py
@@ -24,15 +24,13 @@ class ButtonLink(rst.Directive):
     def run(self):
         url = self.arguments[0]
         text = "\n".join(self.content).strip()
-        html = (
-            f'<div style="margin: 2em 0;">'
-            f'<table border="0" cellpadding="0" cellspacing="0" style="background-color:#fdb913; border-radius:5px; margin:auto;">'
-            f'<tr>'
-            f'<td align="center" valign="middle" style="color:#000000; font-family:Helvetica, Arial, sans-serif; font-size:16px; font-weight:bold; letter-spacing:-.5px; line-height:150%; padding-top:15px; padding-right:30px; padding-bottom:15px; padding-left:30px;">'
-            f'<a href="{url}" target="_blank" style="color:#000000; text-decoration:none; text-transform:uppercase; border-bottom: none;">{text}</a>'
-            f'</td>'
-            f'</tr>'
-            f'</table>'
-            f'</div>'
-        )
+        html = f"""<div style="margin: 2em 0;">
+<table border="0" cellpadding="0" cellspacing="0" style="background-color:#fdb913; border-radius:5px; margin:auto;">
+<tr>
+<td align="center" valign="middle" style="color:#000000; font-family:Helvetica, Arial, sans-serif; font-size:16px; font-weight:bold; letter-spacing:-.5px; line-height:150%; padding-top:15px; padding-right:30px; padding-bottom:15px; padding-left:30px;">
+<a href="{url}" target="_blank" style="color:#000000; text-decoration:none; text-transform:uppercase; border-bottom: none;">{text}</a>
+</td>
+</tr>
+</table>
+</div>"""
         return [nodes.raw("", html, format="html")]
